d73347197d0f4177170f63243c52fe3906ed8139
lizepeng <lizepeng@daling.com>
2020-05-20 11:37:31 +0800

    ....

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mac/programs/flutter-sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/projects/tonal/tonal"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/projects/tonal"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
